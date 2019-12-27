24a71ad88c8cdc64c17cecf26ac870b56951491f
jcombes9834 <jcombes9834@gmail.com>
2019-12-27 21:05:01 +0000

    adding file io

--- tel.py
+++ tel.py
@@ -5,11 +5,22 @@ import time
 
 adxl = ADXL345()
 start = time.time()
-
+alt = [0]
+accel = [0]
 for i in range(100):
-	print(altitude(), adxl.getAxes(False))
+	#alt.append(altitude())
+	#accel.append(adxl.getAxes(False)))
 	now = time.time()
+	with open("data.txt", 'a', encoding = 'utf-8') as f:
+		f.write(str(altitude()))
+		f.write(" ")
+		axes = adxl.getAxes(False)
+		f.write(axes['x']))
+		f.write(" ")
+		f.write(axes['y'])
+		f.write(" ")
+		f.write(axes['z'])
+		f.write("\n")
 	while time.time()-now < 0.1: #wait 100 ms between readings
 		pass
-
 print("It is finished")
