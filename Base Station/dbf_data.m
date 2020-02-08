214c425109d5086aceb3cbf04585adf745e15122
John Combes <jcombes@ufl.edu>
2020-02-08 18:56:58 -0500

    M file for processing data from first two test flights. 2/8/2020

--- Base Station/dbf_data.m
+++ Base Station/dbf_data.m
@@ -1,1 +1,9 @@
-disp("Hello World!");
+T = readtable('dbf test flight one.csv');
+t1 = T.Var1; %first column is the memory address each val was stored in. For the first test flight values were read every 300 milliseconds.
+v1 = T.Var2; %voltage readings from first test flight
+t2 = T.Var3; %memory address vector for second test flight
+v2 = T.Var4; %voltage readings from second test flight.
+
+plot(t1, v1);
+figure()
+plot(t2, v2);
